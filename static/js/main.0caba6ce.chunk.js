1768fc1e40fb468641a00efb026cd161ff57b5cc
dasBarbie <rebecca.hjorvar@outlook.com>
2021-06-02 15:22:15 +0200

    Updates

---
+++ static/js/main.0caba6ce.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[0],{9:function(e,c,t){"use strict";t.r(c);t(1);var r=t(3),n=t.n(r),a=t(0);var s=function(){var e=(new Date).getFullYear();return Object(a.jsx)("footer",{children:Object(a.jsxs)("p",{children:["Copyright \xa9\ufe0f ",e," Rebecca Hj\xf6rvar"]})})};var o=function(){return Object(a.jsx)("div",{className:"container",children:Object(a.jsx)(s,{className:"col col-xl-12 col-lg-12"})})};n.a.render(Object(a.jsx)(o,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.0caba6ce.chunk.js.map
