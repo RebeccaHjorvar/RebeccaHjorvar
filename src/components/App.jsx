134a8fb9aa11d318bb4195499b3a51de7323e4e1
dasBarbie <rebecca.hjorvar@outlook.com>
2021-06-02 14:42:36 +0200

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar"
+import Header from "./Header";
+import Footer from "./Footer"
+
+function App(){
+
+    return <div className="container">
+        <Footer className="col col-xl-12 col-lg-12" />
+    </div>
+}
+
+export default App;  
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Navbar(){
+    return <navigation>
+   <h3>Navigation</h3>
+    </navigation>
+}
+
+export default Navbar;
